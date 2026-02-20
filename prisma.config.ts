ca50a081ff8641a6dc9ce5d5b7b217948e3f8b55
Rhzslya <rhzslyaaa@gmail.com>
2026-02-20 22:48:50 +0700

    started init

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,15 @@
-import "dotenv/config";
-import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
+import dotenv from "@dotenvx/dotenvx";
+import { defineConfig, env } from "@prisma/config";
 
-export default {
+dotenv.config();
+
+export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: env("DATABASE_URL"),
   },
-} satisfies PrismaConfig;
+});
